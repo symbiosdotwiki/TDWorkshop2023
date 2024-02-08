2f85bebaa6fed7d611efeff9a1181d0b13be9ae4
N <nic@symbios.wiki>
2024-02-08 18:38:19 +0100

    Stash Changes

--- ESP32_OSC_NeoPixel_v2/ESP32_OSC_NeoPixel_v2.ino
+++ ESP32_OSC_NeoPixel_v2/ESP32_OSC_NeoPixel_v2.ino
@@ -9,7 +9,6 @@
 //#include <ESP8266WiFi.h>
 //#include <ESP8266mDNS.h>
 //#include <WiFiUdp.h>
-#include <ArduinoOTA.h>
 
 #include <SimpleTimer.h>
 
@@ -19,8 +18,8 @@
 #endif
 
 //WIFI setup
-const char* ssid = "StudioFluora_2G";
-const char* password = "studio!!!fluora111";
+const char* ssid = "Hunter X Hunter IRC Fanclub";
+const char* password = "#AntArc!";
 
 SimpleTimer timer;
 bool otaHold = false;
@@ -40,7 +39,7 @@ OSCErrorCode error;
 byte mac[6];
 
 //NEOPIXELS setup
-#define PIN 15
+#define PIN 23
 #define NUM_LEDS 12
 
 #define BRIGHTNESS 50
@@ -111,24 +110,7 @@ void setup() {
   // No authentication by default
   // ArduinoOTA.setPassword((const char *)"123");
 
-  ArduinoOTA.onStart([]() {
-    //Serial.println("Start");
-  });
-  ArduinoOTA.onEnd([]() {
-    //Serial.println("\nEnd");
-  });
-  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    //Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-  });
-  ArduinoOTA.onError([](ota_error_t error) {
-    //Serial.printf("Error[%u]: ", error);
-    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-    else if (error == OTA_END_ERROR) Serial.println("End Failed");
-  });
-  ArduinoOTA.begin();
+  
   Serial.println("SUCCESS");
   Serial.println("Ready");
   Serial.print("IP address: ");
